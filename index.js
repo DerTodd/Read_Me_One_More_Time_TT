8e490dd237c8a9049f44019c6b832d59516fdc3d
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-10-12 14:48:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, generateMarkdown(data)) {
+    //fs.writeFile(generateMarkdown(data))
+//}
+
+// TODO: Create a function to initialize app
+function init() {
+    //what code fires first
+}
+
+// Function call to initialize app
+init();
